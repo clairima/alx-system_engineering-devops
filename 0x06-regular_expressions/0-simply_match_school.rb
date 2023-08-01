63d16e962a376cfc85442917058c6b84ab1ddbaf
clairima <mertahrima@gmail.com>
2023-08-01 11:14:53 -0700

    0 match school

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,14 +1,3 @@
 #!/usr/bin/env ruby
 # 0-simply_match_school.rb
-
-def match_school(arg)
-  regex = /School/
-  match = arg.scan(regex).join
-  puts match + '$'
-end
-
-if ARGV.empty?
-  puts "Usage: ruby 0-simply_match_school.rb <string>"
-else
-  match_school(ARGV[0])
-end
+puts ARGV[0].scan(/School/).join
