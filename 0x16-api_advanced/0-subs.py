bde8c780331fe92f11fe7126fe747fc4e2a43c9b
clairima <mertahrima@gmail.com>
2023-11-07 20:47:03 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
-    if subreddit is None or type(subreddit) is not str:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
         return 0
-    req = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/clairima)'}).json()
-    subdata = req.get("data", {}).get("subscribers", 0)
-    return subdata
